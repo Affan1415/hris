e721c67b9aeb25634dc7ef302c4e0d43ebb9e9f4
Affan Zahir <Affanzahir26@gmail.com>
2023-12-03 18:05:04 +0500

    done

--- hris/hris/Controllers/projectManagerController.cs
+++ hris/hris/Controllers/projectManagerController.cs
@@ -68,4 +68,26 @@ public class projectManagerController : Controller
 	{
 		return View();
 	}
+
+	[HttpGet]
+	public IActionResult expenses()
+	{
+		return View();
+	}
+
+	[HttpPost]
+	public IActionResult AddExpense(ExpenseDataModel expense)
+	{
+		if (ModelState.IsValid)
+		{
+			_context.ExpenseData.Add(expense);
+			_context.SaveChanges();
+
+			// Redirect to a success page or another action
+			return RedirectToAction("projectManagerdashboard");
+		}
+
+		// If the ModelState is not valid, return to the expense addition form with validation errors
+		return View(expense);
+	}
 }
